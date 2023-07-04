b2879616dc9368eca47c2198b79b6032eca73916
darioang <64955296+darioang@users.noreply.github.com>
2023-07-04 21:01:02 +0800

    Add files via upload

---
+++ craco.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    webpack: {
+      configure: config => ({
+        ...config,
+        module: {
+          ...config.module,
+          rules: config.module.rules.map(rule => {
+            if (rule.oneOf instanceof Array) {
+              rule.oneOf[rule.oneOf.length - 1].exclude = [
+                /\.(js|mjs|jsx|cjs|ts|tsx)$/,
+                /\.html$/,
+                /\.json$/,
+              ];
+            }
+            return rule;
+          }),
+        },
+      }),
+    },
+  };
+  
+  
+  
